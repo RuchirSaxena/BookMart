ff0e70e0bcba7f91cb569155bc1ddb43a193d5d3
preetika2901 <preetika.jain@globallogic.com>
2023-05-05 17:19:00 +0530

    Added filters with hover effect

--- bookmart/src/App.js
+++ bookmart/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import Filter from './components/Filters/Filter';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Romance } from './components/Filters/Romance';
+import { Scifi } from './components/Filters/Scifi';
+import { Mystery } from './components/Filters/Mystery';
+import { Comedy } from './components/Filters/Comedy';
+import { Selfhelp } from './components/Filters/Selfhelp';
+
 
 function App() {
   return (
     <div className="App">
-     Main Component
+    
+      <BrowserRouter>
+      
+      
+      <Filter />
+       
+        <Routes>
+          <Route path="/fiction" element={<Scifi/>} />
+          <Route path="/funny" element={<Comedy />} />
+          <Route path="/love" element={<Romance />} />
+          <Route path="/mystery" element={<Mystery />} />
+          <Route path="/selfhelp" element={<Selfhelp />} /></Routes>
+          
+
+         
+        
+          
+        
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ bookmart/src/components/Filters/Mystery.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Mystery = () => {
+  return (
+    <div>Mystery</div>
+  )
+}
