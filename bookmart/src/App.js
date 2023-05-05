e5047887299e08c6c14a1d83a40a9279500dab5b
Sushant-mishra-21 <sushant.mishra@globallogic.com>
2023-05-05 16:15:46 +0530

    Fetched and displayed data with the help of Firebase

--- bookmart/src/App.js
+++ bookmart/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./components/Home/Index";
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: "# BFCCB5" }}>
       Main Component
       <Home />
     </div>
--- bookmart/src/components/Home/Index.jsx
+++ bookmart/src/components/Home/Index.jsx
@@ -3,8 +3,10 @@ import "./Index.css";
 import Card from "../Card/Index";
 import { db } from "../../firebase";
 import { onSnapshot, collection } from "firebase/firestore";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const Index = () => {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsub = onSnapshot(
@@ -15,6 +17,7 @@ const Index = () => {
           list.push({ id: doc.id, ...doc.data() });
         });
         setBooks(list);
+        setLoading(false);
       },
       (error) => {
         console.log(error);
@@ -28,37 +31,26 @@ const Index = () => {
   console.log(books);
 
   return (
-    <section className="bookContainer">
-      <Card
-        title="Sushant1"
-        price="10"
-        image="https://th.bing.com/th/id/R.c5b2747be236f15c6beead918a0062ef?rik=3xAQFsq44DFsfQ&riu=http%3a%2f%2fimg02.deviantart.net%2febc2%2fi%2f2013%2f159%2f4%2f7%2fpile_of_books_on_a_black_background_by_macinivnw-d68c8a1.jpg&ehk=twfYMiI8Fo2XtATOuCGIreByTSdFFvJ86UsE9LlOQR0%3d&risl=&pid=ImgRaw&r=0"
-        id="01"
-      />
-      <Card
-        title="Sushant2"
-        price="150"
-        image="https://th.bing.com/th/id/OIP.xRo_EoQakakoZ3C3SlEXvgAAAA?pid=ImgDet&w=140&h=212&c=7"
-        id="02"
-      />
-      <Card
-        title="Sushant3"
-        price="1600"
-        image="https://media.istockphoto.com/photos/stack-of-colored-books-isolated-on-white-with-clipping-path-picture-id496954602?k=6&m=496954602&s=612x612&w=0&h=jQwzUYtwiBKzvgKZusMtKpvX4Bt2zII_hc4nCk6x2tU="
-        id="03"
-      />
-      <Card
-        title="Sushant4"
-        price="1600"
-        image="https://media.istockphoto.com/photos/stack-of-colored-books-isolated-on-white-with-clipping-path-picture-id496954602?k=6&m=496954602&s=612x612&w=0&h=jQwzUYtwiBKzvgKZusMtKpvX4Bt2zII_hc4nCk6x2tU="
-        id="04"
-      />
-      <Card
-        title="Sushant5"
-        price="109"
-        image="https://th.bing.com/th/id/R.c5b2747be236f15c6beead918a0062ef?rik=3xAQFsq44DFsfQ&riu=http%3a%2f%2fimg02.deviantart.net%2febc2%2fi%2f2013%2f159%2f4%2f7%2fpile_of_books_on_a_black_background_by_macinivnw-d68c8a1.jpg&ehk=twfYMiI8Fo2XtATOuCGIreByTSdFFvJ86UsE9LlOQR0%3d&risl=&pid=ImgRaw&r=0"
-        id="05"
-      />
+    <section className="bookContainer ">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="row">
+          {books.map((bookItem) => (
+            <div
+              className="col col-12 col-sm-4 col-md-3 col-lg-3"
+              key={bookItem.id}
+            >
+              <Card
+                title={bookItem.name}
+                price={bookItem.priceOffered}
+                image={bookItem.imgURLs[0]}
+                id={bookItem.id}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
