9af83ea080fd1b579d81076a19e99533536927c8
Shruti <shruti.rohila@globallogic.com>
2023-05-05 12:39:07 +0530

    Footer and Header Components were added

--- bookmart/src/App.js
+++ bookmart/src/App.js
@@ -1,10 +1,23 @@
+
 import "./App.css";
 import Home from "./components/Home/Index";
+import Header from "./components/header/Index";
+import Footer from "./components/footer/Index";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
-      Main Component
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        
+     
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- bookmart/src/components/header/Index.jsx
+++ bookmart/src/components/header/Index.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
+import "./Index.css";
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div className="container-fluid mx-2">
+        <NavLink className="navbar-brand" to="/">
+          BOOKMART
+        </NavLink>
+
+        <form className="mx-5">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            size="50"
+          />
+        </form>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse nav-icon" id="collapsibleNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <li className="nav-item">
+              <NavLink className="btn btn-outline-light" exact to="/users/sell">
+                SELL
+              </NavLink>
+              </li>
+            </li>
+            
+            <li className="nav-item">
+              <li className="nav-item">
+              <NavLink className="btn btn-dark" exact to="/users/sell">
+                <FavoriteBorderIcon />
+              </NavLink>
+              </li>
+            </li>
+            <li className="nav-item">
+              <li className="nav-item">
+              <NavLink className="btn btn-dark" exact to="/components/cart">
+                <ShoppingCartOutlinedIcon />
+              </NavLink>
+              </li>
+            </li>
+            <li className="nav-item">
+              <li className="nav-item">
+              <NavLink className="btn btn-dark" exact to="/components/login">
+                <AccountCircleOutlinedIcon />
+              </NavLink>
+              </li>
+            </li>
+          </ul>
+
+          
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Index;
