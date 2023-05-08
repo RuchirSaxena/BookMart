8819a45f9377dedb6213b2c017b46ff2c963f73b
suryanshuverma17 <suryanshu.verma@globallogic.com>
2023-05-08 12:01:33 +0530

    Data is now taken from firebase.

--- bookmart/src/components/Search/SearchBar.js
+++ bookmart/src/components/Search/SearchBar.js
@@ -1,74 +1,80 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./SearchBar.css";
 import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 import { onSnapshot, collection } from "firebase/firestore";
 import { db } from "../../firebase";
 
-const SearchBar = ({ placeholder}) => {
-    const [filteredData,setFilteredData]= useState([]);
-    const [wordEntered,setWordEntered]= useState("");
-    const [books, setBooks] = useState([]);
+const SearchBar = ({ placeholder }) => {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+  const [books, setBooks] = useState([]);
 
-    useEffect(() => {
-      const unsub = onSnapshot(
-        collection(db, "booksmanually"),
-        (snapshot) => {
-          let list = [];
-          snapshot.docs.forEach((doc) => {
-            list.push({ id: doc.id, ...doc.data() });
-          });
-          setBooks(list);
-         
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-      return () => {
-        unsub();
-        
-      };
-    }, []);
-    
-    const handleFilter = (event) => {
-     const searchWord  = event.target.value;
-     setWordEntered(searchWord)
-     const  newFilter = books.filter((value) => {
-        return value.name.toLowerCase().includes(searchWord.toLowerCase());
-     });
-
-     if(searchWord === ""){
-        setFilteredData([])
-     }else{
-        setFilteredData(newFilter);
-     }
+  useEffect(() => {
+    const unsub = onSnapshot(
+      collection(db, "booksmanually"),
+      (snapshot) => {
+        let list = [];
+        snapshot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setBooks(list);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    return () => {
+      unsub();
     };
-    const clearInput = () => {
-        setFilteredData([]);
-        setWordEntered("");
+  }, []);
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = books.filter((value) => {
+      return value.name.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
     }
-   
+  };
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} value={wordEntered} onChange= {handleFilter}/>
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
         <div className="searchIcon">
-            {wordEntered.length === 0?    <SearchIcon />: <CloseIcon id="clearBtn" onClick={clearInput}/> }
-       
+          {wordEntered.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput} />
+          )}
         </div>
       </div>
-      {filteredData.length !==0 && (
-      <div className="dataResult">
-        {filteredData.slice(0,15).map((value, key) => {
-          return (
-            <div className="dataItem">
-            <p>{value.name}</p>
-            </div>
-          );
-        })}
-      </div>
-)}
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <div className="dataItem">
+                <p>{value.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
