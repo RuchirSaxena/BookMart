217ab4b26c187a745258a6d61ce9ba46ab75b27b
Sushant-mishra-21 <sushant.mishra@globallogic.com>
2023-05-09 13:07:45 +0530

    commiting the Filter Component with on Click Shadow effect

--- bookmart/src/components/Filter/Index.jsx
+++ bookmart/src/components/Filter/Index.jsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Card from "../Card/Index";
 import "./Index.css";
-import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 const Index = (props) => {
   const [filtered, setFiltered] = useState([]);
-
+  const [filter, setFilter] = useState("");
   const handleCategorySelect = (category) => {
     const filteredBooks = props.books.filter(
       (book) => book.category === category
     );
+
     setFiltered(filteredBooks);
-    
+    setFilter(category);
   };
 
   return (
@@ -18,72 +19,113 @@ const Index = (props) => {
       <div className="row">
         <div className="firstAnimation col-6 col-sm-6 col-md-3 col-lg-2 m-2">
           <div className="secondAnimation">
-            <div className="card1">
+            <div
+              className={
+                filter === "Self Help" ? "cardContainer active" : "cardContainer"
+              }
+              onClick={() => handleCategorySelect("Self Help")}
+            >
               <img
                 src="https://magazine.washington.edu/columns_wordpress/wp-content/uploads/2021/11/selfhelp2k.jpg"
                 alt="Self Help"
-                onClick={() => handleCategorySelect("Self Help")}
               />
+              <p>Self Help</p>
             </div>
           </div>
         </div>
         <div className="firstAnimation col-6  col-sm-6 col-md-3 col-lg-2 m-2">
           <div className="secondAnimation">
-            <div className="card1">
+            <div
+              className={
+                filter === "Education" ? "cardContainer active" : "cardContainer"
+              }
+              onClick={() => handleCategorySelect("Education")}
+            >
               <img
                 src="https://img.freepik.com/premium-photo/photograph-wooden-table-with-vintage-book-open-it_410516-14808.jpg?w=740"
                 alt="Mystery"
-                onClick={() => handleCategorySelect("Education")}
               />
+              <p>Education</p>
             </div>
           </div>
         </div>
         <div className="firstAnimation col-6 col-sm-6 col-md-3 col-lg-2 m-2">
           <div className="secondAnimation">
-            <div className="card1">
+            <div
+              className={
+                filter === "Romance" ? "cardContainer active" : "cardContainer"
+              }
+              onClick={() => handleCategorySelect("Romance")}
+            >
               <img
                 src="https://img.freepik.com/free-photo/red-rose-inside-open-book_181624-28498.jpg?w=740&t=st=1683527586~exp=1683528186~hmac=864ca4da2d7d3a07b23c35d5c1eb202363f6e9779b7a5641538930b260cf7af0"
                 alt="Romance"
-                onClick={() => handleCategorySelect("Romance")}
               />
+              <p>Romance</p>
             </div>
           </div>
         </div>
         <div className="firstAnimation col-6 col-sm-6 col-md-3 col-lg-2 m-2">
           <div className="secondAnimation">
-            <div className="card1">
+            <div
+              className={
+                filter === "Comedy" ? "cardContainer active" : "cardContainer"
+              }
+              onClick={() => {
+                handleCategorySelect("Comedy");
+              }}
+            >
               <img
                 src="https://media.npr.org/assets/img/2012/07/16/humor-57edbd64049db5b1b2c321a612e07a7e734bbe25-s1100-c50.jpg"
                 alt="Comedy"
-                onClick={() => handleCategorySelect("Comedy")}
               />
+              <p>Comedy</p>
             </div>
           </div>
         </div>
         <div className="firstAnimation col-6 col-sm-6 col-md-3 col-lg-2 m-2">
           <div className="secondAnimation">
-            <div className="card1">
+            <div
+              className={
+                filter === "Fiction" ? "cardContainer active" : "cardContainer"
+              }
+              onClick={() => handleCategorySelect("Fiction")}
+            >
               <img
                 src="https://static.toiimg.com/thumb/58907558.cms?width=680&height=512&imgsize=199255"
                 alt="Science-fictional"
-                onClick={() => handleCategorySelect("Fiction")}
               />
+              <p>Fiction</p>
             </div>
           </div>
         </div>
       </div>
-    
+      <div>
+        <button
+          onClick={() => {
+            setFiltered([]);
+            setFilter("");
+          }}
+        >
+          CLear Filter
+        </button>
+      </div>
       <div className="filteredData">
-        
-        {filtered.map((book) => (
-          <Card
-            key={book.id}
-            title={book.name}
-            author={book.author}
-            image={book.imgURLs[0]}
-            price={book.priceOffered}
-          />
-        ))}
+        <div className="row justify-content-around">
+          {filtered.map((bookItem) => (
+            <div
+              className="col col-sm-6 col-md-3 col-lg-2 m-2 "
+              key={bookItem.id}
+            >
+              <Card
+                title={bookItem.name}
+                price={bookItem.priceOffered}
+                image={bookItem.imgURLs[0]}
+                id={bookItem.id}
+              />
+            </div>
+          ))}
+        </div>
       </div>
       <hr />
     </div>
