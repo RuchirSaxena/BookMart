eaf3b274a401f1dd59884b589f45264df8f62c36
shrutirohila <126046021+shrutirohila@users.noreply.github.com>
2023-05-11 10:34:45 +0530

    Update App.js

--- bookmart/src/App.js
+++ bookmart/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import Home from "./components/Home/Index";
+import Home from "./components/Home/index";
 
-import Header from "./components/header/Index";
-import Footer from "./components/footer/Index";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
 import Productdetails from "./components/Product-details/Productdetails";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
