2b505b8ec7ccd18eea6147b483b9a79fed550d8d
suryanshuverma17 <suryanshu.verma@globallogic.com>
2023-05-12 16:20:04 +0530

    BM-02 Changes done .

---
+++ bookmart/src/components/Utilities/Constants.js
@@ -0,0 +1,15 @@
+const Constants = {
+  createAccount: "Create Account",
+  name: "Your Name",
+  email: "Email",
+  password: "Password",
+  phoneNumber: "Phone Number",
+  signUp: "Sign Up",
+  signIn: "Sign In",
+  accountCheck: "Already Have an account?",
+  successMsg: "New User added Successfully",
+  errorMsg: "User Already Exists",
+  missingFields: "Please Fill all required fields",
+};
+
+export default Constants;
