43a72b29520fd3b140f0bb1a0df7e68e04f97a3e
Sparsh Mittal <sparsh.mittal@globallogic.com>
2023-05-22 10:36:03 +0530

    fix commit

--- bookmart/src/components/header/Index.jsx
+++ bookmart/src/components/header/Index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./index.css";
+import "./Index.css";
 import Searchbar from "./Searchbar";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
